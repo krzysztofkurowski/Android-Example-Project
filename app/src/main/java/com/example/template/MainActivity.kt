e35bd2c3680910ae54f81d200d96eeb83e51d453
Krzysztof <krzysztof.kurowski@payair.com>
2020-01-27 16:18:08 +0100

    add back arrow

--- app/src/main/java/com/example/template/MainActivity.kt
+++ app/src/main/java/com/example/template/MainActivity.kt
@@ -2,13 +2,32 @@ package com.example.template
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
+    private val appBarConfiguration by lazy {
+        AppBarConfiguration.Builder(navController.graph).build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        navController = findNavController(R.id.nav_host_fragment)
+        setupActionBar()
     }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp() = NavigationUI.navigateUp(navController, appBarConfiguration)
 }
